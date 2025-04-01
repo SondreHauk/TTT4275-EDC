c9c2e104b0226b9559c6720d4d595e5b349e7b89
John Birger Morud <johnbirgermorud@dhcp-10-22-161-106.wlan.ntnu.no>
2025-04-01 16:47:54 +0200

    almost implemented nearest neighbor

--- Problem 2/main.m
+++ Problem 2/main.m
@@ -18,7 +18,6 @@ chunk_len = 1000;
 %% Nearest neighboor clasification
 
 nearest_neighbor = -1*ones(N_te, 1);
-
 nearest_neighbor_temp_dist =  inf(chunk_len, 1);
 nearest_neighbor_temp_label = -1 * ones(chunk_len,1);
 
@@ -27,13 +26,23 @@ for i = 0:(N_te/chunk_len - 1)
     chunk_test_data = test_data(chunk_len*i+1 : chunk_len*(i+1), :);
 
     for j = 0:(N_tr/chunk_len - 1)
-        chunk_training_data = training_data(chunk_len*i+1 : chunk_len*(i+1), :);
-        chunk_label = training_label(chunk_len*i+1 : chunk_len*(i+1), :);
-        E = dist(chunk_training_data, chunk_test_data');
+        chunk_training_data = training_data(chunk_len*j+1 : chunk_len*(j+1), :);
+        chunk_training_label = training_label(chunk_len*j+1 : chunk_len*(j+1), :);
+        
+        % E = dist(chunk_training_data, chunk_test_data');
+        E = dist(chunk_test_data, chunk_training_data');
         [distance, idx] = min(E, [], 2);
-        disp()
         
+        update_idx = distance < nearest_neighbor_temp_dist;
+        nearest_neighbor_temp_dist(update_idx) = distance(update_idx);        
+        nearest_neighbor_temp_label(update_idx) = chunk_training_label(update_idx);
         
-       
     end
+    fprintf("[NN, line 40] Temp label\n");
+    disp(nearest_neighbor_temp_label);
+    disp("")
+
+    nearest_neighbor(i*chunk_len+1:(i+1)*chunk_len) = nearest_neighbor_temp_label;
+    nearest_neighbor_temp_dist =  inf(chunk_len, 1);
+    nearest_neighbor_temp_label = -1 * ones(chunk_len,1);
 end
